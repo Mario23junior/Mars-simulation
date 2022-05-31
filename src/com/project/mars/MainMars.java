d285d8503908690ad2ef270ea70d71554e066b0d
Mário Junior <48899365+Mario23junior@users.noreply.github.com>
2022-05-31 02:49:47 -0300

    Criando esfera experimental

--- src/com/project/mars/MainMars.java
+++ src/com/project/mars/MainMars.java
@@ -1,14 +1,31 @@
 package com.project.mars;
 
+
 import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.shape.Sphere;
 import javafx.stage.Stage;
 
 public class MainMars extends Application{
 
 	@Override
-	public void start(Stage arg0) throws Exception {
-		// TODO Auto-generated method stub
+	public void start(Stage primaryStage) throws Exception {
+		Sphere sphere = new Sphere(50);
 		
+		Group group = new Group();
+		group.getChildren().add(sphere);
+		
+		Scene scene = new Scene(group,1400,800);
+		
+		primaryStage.setTitle("Planeta Mars");
+		primaryStage.setScene(scene);
+		primaryStage.show();
+		
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
 	}
 
 } 
