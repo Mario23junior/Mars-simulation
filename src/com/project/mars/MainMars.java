ec571bc73ccf0163040bc3db6559973b0028e725
Mário Junior <48899365+Mario23junior@users.noreply.github.com>
2022-05-31 02:59:09 -0300

    Posicionando estrutura da esfera
    Posicionando estrutura da esfera com base na resolução do monitor

--- src/com/project/mars/MainMars.java
+++ src/com/project/mars/MainMars.java
@@ -9,6 +9,10 @@ import javafx.stage.Stage;
 
 public class MainMars extends Application{
 
+	private static final Integer WIDTH = 1400;
+	private static final Integer HEIGHT = 800;
+
+	
 	@Override
 	public void start(Stage primaryStage) throws Exception {
 		Sphere sphere = new Sphere(50);
@@ -16,7 +20,9 @@ public class MainMars extends Application{
 		Group group = new Group();
 		group.getChildren().add(sphere);
 		
-		Scene scene = new Scene(group,1400,800);
+		Scene scene = new Scene(group,WIDTH,HEIGHT);
+		
+		sphere.translateXProperty().set(WIDTH / 2);
 		
 		primaryStage.setTitle("Planeta Mars");
 		primaryStage.setScene(scene);
